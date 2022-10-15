e27e6e8f172e660af7f5d0134a8594b1c35677c1
Onwubiko Okem Precious <onwubikookem@gmail.com>
2022-10-14 18:24:37 -0700

    third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Print all the letters of alphabet in lowercase
+* Return: always 0
+*/
+
+int main(void)
+{
+	int ch
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
